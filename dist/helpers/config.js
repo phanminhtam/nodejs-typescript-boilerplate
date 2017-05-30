876555b77a61fd2a2667d8f55d0da7e0d6c94dc5
TamPhan <phanminhtam1992@gmail.com>
2017-05-30 14:26:11 +0700

    update gitignore

--- dist/helpers/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-var config = require('../../config.json');
-/**
- * Config
- */
-exports.default = config;
-//# sourceMappingURL=config.js.map
--- dist/helpers/helper.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-var Helper = (function () {
-    function Helper() {
-    }
-    return Helper;
-}());
-exports.default = Helper;
-//# sourceMappingURL=helper.js.map
